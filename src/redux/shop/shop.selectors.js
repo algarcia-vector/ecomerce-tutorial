40d4e2e4d905728923a7591e754c288ccb94a22a
unknown <algarcia@vector.com.mx>
2021-04-21 13:07:45 -0500

    moved our directory and shop data into own reducer, created respective selectors and updated corresponding components with new redux flow for directory/shop

---
+++ src/redux/shop/shop.selectors.js
@@ -0,0 +1,37 @@
+import { createSelector } from "reselect";
+
+const selectShop = (state) => state.shop;
+
+export const selectCollections = createSelector(
+  [selectShop],
+  (shop) => shop.collections
+);
+
+export const selectCollectionsForPreview = createSelector(
+  [selectCollections],
+  (collections) => Object.keys(collections).map((key) => collections[key])
+);
+
+export const selectCollection = (collectionUrlParam) =>
+  createSelector(
+    [selectCollections],
+    (collections) => collections[collectionUrlParam]
+  );
+
+// import memoize from "lodash.memoize";
+
+// const COLLECTION_ID_MAP = {
+//   hats: 1,
+//   sneakers: 2,
+//   jackets: 3,
+//   womens: 4,
+//   mens: 5,
+// };
+
+// export const selectCollection = memoize((collectionUrlParam) =>
+//   createSelector([selectCollections], (collections) =>
+//     collections.find(
+//       (collections) => collections.id === COLLECTION_ID_MAP[collectionUrlParam]
+//     )
+//   )
+// );
